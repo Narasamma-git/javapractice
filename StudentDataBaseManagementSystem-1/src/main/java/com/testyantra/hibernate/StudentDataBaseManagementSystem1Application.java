2d7d376613f34b1ce6734831ba1f6edbec674bc1
Narasamma-git <87521821+Narasamma-git@users.noreply.github.com>
2021-12-14 10:12:22 +0530

    fullstack

---
+++ StudentDataBaseManagementSystem-1/src/main/java/com/testyantra/hibernate/StudentDataBaseManagementSystem1Application.java
@@ -0,0 +1,13 @@
+package com.testyantra.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDataBaseManagementSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDataBaseManagementSystem1Application.class, args);
+	}
+
+}
---
+++ StudentDataBaseManagementSystem-1/src/test/java/com/testyantra/hibernate/StudentDataBaseManagementSystem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testyantra.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDataBaseManagementSystem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
